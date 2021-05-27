fe001f1a811bd3302270d26cedc23c10d3641ccd
YuanChang <759337828@qq.com>
2021-05-27 14:24:33 +0800

    errors

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,4 @@
 class MainController < ApplicationController
   def index
-    flash.now[:notice] = "Login successful"
-    flash.now[:alert] = "Login alert"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'about-us', to: 'about#index', as: :about
+  get 'about_us', to: 'about#index', as: :about
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
   root to: 'main#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
